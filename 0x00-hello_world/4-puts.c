dd5f86187454f5874b6c463c9b2aa4c375a53e64
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-02-16 12:47:10 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,20 @@
-#iclude <stdio.h>
+#include <stdio.h>
 /**
  * main - main block
- * return: 0
+ * Return: 0
  */
-int main (void)
+int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-	return (0);  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
+
+
+
+
+
+
+
+
 

e2f116ac4d9913882cf6e94fa7e617aca95f1165
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-02-17 10:34:30 +0000

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,19 +10,19 @@ int main(void)
 {
         int n;
 	if(n > 0)
-	{
+
 	printf("%d is positive\n");	
-	}	
+	
 
 	else if(n = 0)
-	{
+
 	printf("%d is zero\n");
-	}
+	
 
 	else (n < 0)
-	{
+
 	printf("%d is negative\n");
-	}
+
 	return 0;
 }
 

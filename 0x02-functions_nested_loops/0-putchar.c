ceee4405aaaf7107a5f7f1ef000e69cd03d77f19
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-02-21 15:44:49 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,15 +12,16 @@
  */
 int main(void)
 {
-     print("h");
-     print("o");
-     print("b");
-     print("e");
-     print("r");
-     print("t");
-     print("o");
-     print("n");
-     print("\n");
+        _putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
      return (0);
 }     

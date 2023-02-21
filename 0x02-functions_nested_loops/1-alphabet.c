e86bbac9ca2c5c09092a2280dcbffd82700404f8
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-02-21 16:01:09 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#iclude "holberton.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowecase followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		
+	_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}

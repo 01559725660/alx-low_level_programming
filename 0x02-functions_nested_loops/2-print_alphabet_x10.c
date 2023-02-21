186a9149417c7249eec71c50c858865c5132ba9a
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-02-21 16:39:45 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "hoberton.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -10,3 +11,21 @@ int main(void)
 	print_alphabet_x10();
 	return (0);
 }
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}	
